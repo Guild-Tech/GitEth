6370d4068b373487d8e198abc3767cbcb7149d44
johnsmccain <johnsdanlami@gmail.com>
2024-12-06 20:00:28 +0100

    Fix profile button for smooth navigation

--- Frontend-team/src/components/Layout/Navbar.tsx
+++ Frontend-team/src/components/Layout/Navbar.tsx
@@ -6,10 +6,12 @@ import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '@/store';
 // import { DarkThemeToggle } from 'flowbite-react';
 import notificationIcon from "../../assets/icons/notificationIcon.svg"
-import { logout } from '@/store/actions/auth';
+import { loginWithGitHub, logout } from '@/store/actions/auth';
 export default function Navigation() {
   const [activeLink, setActiveLink] = useState('Home'); // Initial active link
   const { user } = useSelector((state: RootState) => state.auth);
+  const [isProfile, setIsProfile] = useState(false);
+
   const dispatch = useAppDispatch()
 
   const navigate = useNavigate();
@@ -21,12 +23,17 @@ export default function Navigation() {
       </Navbar.Brand>
       <div className="flex md:order-2 gap-2">
 
-        {user ? <div className="flex gap-5">
+        {user ? <div className="flex gap-5 relative">
           <CustomBtn text={``} colored="yes" icon={notificationIcon} btnstyle="p-0" iconStyle="w-3 h-3  rounded-full" style="flex w-[48px] h-[48px] max-sm:w-[32px] max-sm:h-[32px]" onClick={() => { navigate("/notifications") }} />
-          <CustomBtn text={`${user?.username}`} icon={user.photoURL} iconStyle="w-8 h-8 rounded-full"  colored="yes" style="hidden md:flex w-[137px] text-[16px]" onClick={() => { dispatch(logout())}} />
-          <CustomBtn text={``} icon={user.photoURL} btnstyle="p-0" iconStyle="w-8 h-8 rounded-full"  colored="yes" style="md:hidden flex max-sm:w-fit max-sm:h-fit text-[16px]" onClick={() => { dispatch(logout())}} />
+          <CustomBtn text={`${user?.username?.slice(0, 9)}...`} icon={user.photoURL} iconStyle="w-8 h-8 rounded-full" colored="yes" style="hidden md:flex w-[137px] text-[16px]" onClick={() => setIsProfile(!isProfile)} />
+          <CustomBtn text={``} icon={user.photoURL} btnstyle="p-0" iconStyle="w-8 h-8 rounded-full" colored="yes" style="md:hidden flex max-sm:w-fit max-sm:h-fit text-[16px]" onClick={() => setIsProfile(!isProfile)} />
+          {isProfile && <div className="absolute top-12 right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
+            <Link to="/profile" onClick={()=> setIsProfile(false)} className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</Link>
+            <a href="#" onClick={()=> setIsProfile(false)} className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2" onClick={() =>{ dispatch(logout()); setIsProfile(false)}}>Sign out</a>
+          </div>}
         </div> :
-          <CustomBtn text={`Login with Github`} colored="yes" style="hidden md:flex" onClick={() => { navigate("/onboarding") }} />
+          <CustomBtn text={`Login with Github`} colored="yes" style="hidden md:flex" onClick={() => { user ? dispatch(loginWithGitHub({ role: user })) : navigate("/onboarding") }} />
         }
         {/* <DarkThemeToggle /> */}
         <Navbar.Toggle className='w-8 h-8 flex items-center justify-center ' />
